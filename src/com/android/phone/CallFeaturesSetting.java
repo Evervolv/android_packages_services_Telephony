74fdcea4bc1d8866950af470754daa3215293b8f
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-11 17:26:14 +0700

    Telephony: fix MSim package names
    Broke in a4ae7bacf1ec564343540f3687505f3911796946
    
    Change-Id: I77072846a3db561521cca8bbb5e8b9e20dcc3232

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -1742,7 +1742,7 @@ public class CallFeaturesSetting extends PreferenceActivity
             Intent intent = selectSub.getIntent();
             intent.putExtra(SelectSubscription.PACKAGE, "com.android.phone");
             intent.putExtra(SelectSubscription.TARGET_CLASS,
-                    "com.android.phone.MSimCallFeaturesSubSetting");
+                    "com.android.phone.msim.MSimCallFeaturesSubSetting");
         }
 
         if (isMsim && TelephonyManager.getDefault().getMultiSimConfiguration() !=
